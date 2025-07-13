31d753567b44d3131393e6bc3573664ef1ec1025
Rheetham Menon <rheethammenon@gmail.com>
2025-07-13 18:15:15 +0530

    Configured entity, dao for read only REST api

---
+++ 2-backend/spring-boot-library/src/main/java/com/rheetham/spring_boot_library/config/MyDataRestConfig.java
@@ -0,0 +1,42 @@
+package com.rheetham.spring_boot_library.config;
+
+import com.rheetham.spring_boot_library.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+                                                     CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {
+                HttpMethod.POST,
+                HttpMethod.PUT,
+                HttpMethod.DELETE,
+                HttpMethod.PATCH
+        };
+
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class, config, theUnsupportedActions);
+
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+
+    public void disableHttpMethods(Class theClass,
+                                   RepositoryRestConfiguration config,
+                                   HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure((metdata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) ->
+                        httpMethods.disable(theUnsupportedActions));
+    }
+}
---
+++ 2-backend/spring-boot-library/src/main/java/com/rheetham/spring_boot_library/dao/BookRepository.java
@@ -0,0 +1,7 @@
+package com.rheetham.spring_boot_library.dao;
+
+import com.rheetham.spring_boot_library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
