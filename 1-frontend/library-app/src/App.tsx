0d820c5125ab8a020da2e2c1985025197a98e08f
Rheetham Menon <rheethammenon@gmail.com>
2025-07-17 20:04:49 +0530

    Added Search Books page with search book component and pagination util

--- 1-frontend/library-app/src/App.tsx
+++ 1-frontend/library-app/src/App.tsx
@@ -3,12 +3,13 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <HomePage/>
+      <SearchBooksPage/>
       <Footer/>
     </div>
   );
